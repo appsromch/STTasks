013531d52de8de60abb8281e8af5e8ce509d52ad
Sergey Tyo <sergey.tyo.apps@gmail.com>
2013-08-26 10:50:36 +0500

    [release] ST Tasks v1.0

--- STTasks/Views/STTaskCell.m
+++ STTasks/Views/STTaskCell.m
@@ -25,4 +25,26 @@
 
     // Configure the view for the selected state
 }
+
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated{
+    [super setEditing:editing animated:animated];
+    if (editing&&!self.showingDeleteConfirmation) {
+        [UIView animateWithDuration:0.3
+                         animations:^{
+                             self.imageViewPicture.transform=CGAffineTransformMakeTranslation(80, 0);
+                             self.labelTitle.transform=CGAffineTransformMakeTranslation(80, 0);
+                             self.labelInfo.transform=CGAffineTransformMakeTranslation(80, 0);
+                         }];
+    }
+    else{
+        [UIView animateWithDuration:0.3
+                         animations:^{
+                             self.imageViewPicture.transform=CGAffineTransformIdentity;
+                             self.labelTitle.transform=CGAffineTransformIdentity;
+                             self.labelInfo.transform=CGAffineTransformIdentity;
+                         }];
+    }
+}
+
+                                
 @end
